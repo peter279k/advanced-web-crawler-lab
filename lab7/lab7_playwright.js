42c114e0103668aac2372f2a56b275b85d668bb4
peter279k <peter279k@gmail.com>
2023-11-01 11:08:19 +0800

    Add Lab7 example about Node.js with Playwright

---
+++ lab7/lab7_playwright.js
@@ -0,0 +1,22 @@
+// playwright-extra is a drop-in replacement for playwright,
+// it augments the installed playwright with plugin functionality
+import { chromium } from 'playwright-extra';
+
+// Load the stealth plugin and use defaults (all tricks to hide playwright usage)
+// Note: playwright-extra is compatible with most puppeteer-extra plugins
+import stealth from 'puppeteer-extra-plugin-stealth';
+
+// Add the plugin to Playwright (any number of plugins can be added)
+chromium.use(stealth());
+
+// That's it. The rest is Playwright usage as normal 😊
+chromium.launch({ headless: false }).then(async (browser) => {
+  const page = await browser.newPage();
+
+  console.log('Testing the stealth plugin..');
+  await page.goto('https://dcard.tw', { waitUntil: 'networkidle' });
+
+  console.log('All done, check the page content. ✨');
+  console.log(await page.content());
+  await browser.close();
+});
